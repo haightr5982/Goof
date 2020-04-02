cdaf55ab09a678913b31589df6ac1d068d50e577
haightr5982 <63072298+haightr5982@users.noreply.github.com>
2020-04-02 15:42:28 -0400

    Add files via upload
    First submission

---
+++ P2T1_SalesPrediction_HaightRichard.py
@@ -0,0 +1,17 @@
+
+# Calculate the profit as 23 persent of total sales
+# 04/01/2020
+# CTI-110 P2T1 - Sales Prediction
+# Richard Haight
+
+
+
+
+#Get the projected total sales
+total_sales = float(input('Enter the projected sales: '))
+
+#Calculate the profit as 23 persent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit
+print ('The profit is $', format(profit, ',.2f'))
